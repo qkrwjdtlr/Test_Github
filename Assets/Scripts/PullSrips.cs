95166d40956229034eef36a3b24a7f80d129a548
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-04-02 14:29:03 +0900

    TT

---
+++ Assets/Scripts/PullSrips.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PullSrips : MonoBehaviour
+{
+    public Vector3 position;
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Vector3 TESTVECTOR;
     void Start()
     {
         
