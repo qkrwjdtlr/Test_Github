6d7d208d788b095b5e84721ae3a563555e85edaa
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-04-02 14:18:16 +0900

    tm

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("ok!!");
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
