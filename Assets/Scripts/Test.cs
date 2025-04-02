7944588d417bb4b30c6f51ad51ffa430dccf7671
qkrwjdtlr <132883478+qkrwjdtlr@users.noreply.github.com>
2025-04-02 14:29:51 +0900

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-  public Vector3 TT;
+  public Vector3 TT3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
